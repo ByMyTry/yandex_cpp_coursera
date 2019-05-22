453ed961666b087d91e678f8cdd62ff422668798
Anton Taleckij <anton.taleckij@gmail.com>
2019-05-22 22:58:20 +0300

    woking on third week

--- 0_white/week3/incognizable.cpp
+++ 0_white/week3/incognizable.cpp
@@ -24,9 +24,9 @@ class Incognizable{
 };
 
 int main() {
-  Incognizable a;
-  Incognizable b = {};
-  Incognizable c = {0};
-  Incognizable d = {0, 1};
-  return 0;
+    Incognizable a;
+    Incognizable b = {};
+    Incognizable c = {0};
+    Incognizable d = {0, 1};
+    return 0;
 }
